8f889769fc854211213f44b71d3d584b2a0155f7
djalma <djalma21cruz@gmail.com>
2019-01-09 10:11:58 -0300

    comit inicial

---
+++ calculo.js
@@ -0,0 +1,88 @@
+// Javascript document
+
+
+//if 
+//var numero1 = 1, 2, 3, 4, 5;
+//var numero2 = 1, 2, 3, 4, 5;
+//var numero3 = 1, 2, 3, 4, 5;
+//var numero4 = 1, 2, 3, 4, 5;
+//var numero5 = 1, 2, 3, 4, 5;
+
+function calculo(){
+var numero1 = Number(document.getElementById("campo1").value);	
+var numero2 = Number(document.getElementById("campo2").value);	
+var numero3 = Number(document.getElementById("campo3").value);
+var numero4 = Number(document.getElementById("campo4").value);
+var numero5 = Number(document.getElementById("campo5").value);
+
+
+var resultado = numero1 + numero2 + numero3 + numero4 + numero5;
+
+document.getElementById("resultado").value = resultado/5;
+
+}
+function calculo1(){
+var numero6 = Number(document.getElementById("campo6").value);	
+var numero7 = Number(document.getElementById("campo7").value);	
+var numero8 = Number(document.getElementById("campo8").value);
+var numero9 = Number(document.getElementById("campo9").value);
+var numero10 = Number(document.getElementById("campo10").value);
+
+var resultado1 = numero6 + numero7 + numero8 + numero9 + numero10;
+
+document.getElementById("resultado1").value = resultado1/5;
+
+}
+function calculo2(){
+var numero11 = Number(document.getElementById("campo11").value);	
+var numero12 = Number(document.getElementById("campo12").value);	
+var numero13 = Number(document.getElementById("campo13").value);
+var numero14 = Number(document.getElementById("campo14").value);
+var numero15 = Number(document.getElementById("campo15").value);
+
+var resultado2 = numero11 + numero12 + numero13 + numero14 + numero15;
+
+document.getElementById("resultado2").value = resultado2/5;
+
+}
+function calculo3(){
+var numero16 = Number(document.getElementById("campo16").value);	
+var numero17 = Number(document.getElementById("campo17").value);	
+var numero18 = Number(document.getElementById("campo18").value);
+var numero19 = Number(document.getElementById("campo19").value);
+var numero20 = Number(document.getElementById("campo20").value);
+
+var resultado3 = numero16 + numero17 + numero18 + numero19 + numero20;
+
+document.getElementById("resultado3").value = resultado3/5;
+
+}
+
+function calculo4(){
+var numero1 = Number(document.getElementById("campo1").value);	
+var numero2 = Number(document.getElementById("campo2").value);	
+var numero3 = Number(document.getElementById("campo3").value);
+var numero4 = Number(document.getElementById("campo4").value);
+var numero5 = Number(document.getElementById("campo5").value);
+var numero6 = Number(document.getElementById("campo6").value);	
+var numero7 = Number(document.getElementById("campo7").value);	
+var numero8 = Number(document.getElementById("campo8").value);
+var numero9 = Number(document.getElementById("campo9").value);
+var numero10 = Number(document.getElementById("campo10").value);
+var numero11 = Number(document.getElementById("campo11").value);	
+var numero12 = Number(document.getElementById("campo12").value);	
+var numero13 = Number(document.getElementById("campo13").value);
+var numero14 = Number(document.getElementById("campo14").value);
+var numero15 = Number(document.getElementById("campo15").value);
+var numero16 = Number(document.getElementById("campo16").value);	
+var numero17 = Number(document.getElementById("campo17").value);	
+var numero18 = Number(document.getElementById("campo18").value);
+var numero19 = Number(document.getElementById("campo19").value);
+var numero20 = Number(document.getElementById("campo20").value);
+
+var resultado4 = numero1 + numero2 + numero3 + numero4 + numero5 + numero6 + numero7 + numero8 + numero9 + numero10 + numero11 + numero12 + numero13 + numero14 + numero15 + numero16 + numero17 + numero18 + numero19 + numero20;
+
+document.getElementById("resultado4").value = resultado4/20;
+
+}
+
